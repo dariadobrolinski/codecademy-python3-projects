8812ca2ef12aabde7119feb125cb994c2ff3de1b
dariadobrolinski <dsdobrolinski@gmail.com>
2025-03-10 10:34:17 -0400

    Add block letters and lovely loveseat project implementations

---
+++ blockletters.py
@@ -0,0 +1,15 @@
+# Block Letters Codecademy project from Python 3 course.
+
+# Name: Daria Dobrolinski
+
+initials  = """
+DDDD    DDDD
+D   D   D   D
+D   D   D   D
+D   D   D   D 
+D   D   D   D 
+D   D   D   D 
+DDDD    DDDD
+"""
+
+print(initials)
